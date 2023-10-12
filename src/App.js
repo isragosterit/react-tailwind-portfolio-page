5125230103ac0b7ed5a6ff03dc9398912e2200ee
İsra Gösterit <82115269+isragosterit@users.noreply.github.com>
2023-10-13 01:01:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+ 
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
