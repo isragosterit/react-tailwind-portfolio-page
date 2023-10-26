8d36a6f2fa8f9b51eb32718bc81f6af3c6936f2f
İsra Gösterit <82115269+isragosterit@users.noreply.github.com>
2023-10-27 00:06:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
- 
+import About from './components/About';
+import Contact from './components/Contact';
+
 export default function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
