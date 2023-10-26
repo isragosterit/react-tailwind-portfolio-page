69299eb48f7de03a3b0406e99d28d61ea7636188
İsra Gösterit <82115269+isragosterit@users.noreply.github.com>
2023-10-27 00:05:24 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,20 +9,19 @@ const Navbar = () => {
   }
 
   return (
-    <section className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>İsra</h1>
-        <ul className='hidden md:flex'>
-        <li className='p-4 hover-cursor'>Home</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+    <section className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#5e9c88] '>
+        <ul className='hidden md:flex mx-auto'>
+        <li className='p-4 hover-cursor hover:text-white hover:cursor-pointer'>Home</li>
+        <li className='p-4 hover:text-white hover:cursor-pointer'>About</li>
+        <li className='p-4 hover:text-white hover:cursor-pointer'>Contact</li>
       </ul>
 
-      <div onClick={handleNav} className='block md:hidden'>
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div onClick={handleNav} className='block md:hidden '>
+        {!nav ? <AiOutlineClose size={20} className='' /> : <AiOutlineMenu size={20} />}
       </div>
      
-      <div className={!nav ? 'fixed left-0 top-0 m-4 w-[60%] h-full border-r border-r-gray-900 bg-[#0e0a1c] text-pink-600 ease-in-out duration-500' : 'fixed left-[-100%]' }>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>İsra</h1>
+      <div className={!nav ? 'hidden fixed left-0 top-0  w-[55%] h-full border-r border-r-gray-900 bg-[#0e0a1c] text-pink-600 ease-in-out duration-500' : 'fixed left-[-100%]' }>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a] m-8'>İsra</h1>
         <ul className='pt-12'>
           <li className='p-4 border-b border-gray-600'>Home</li>
           <li className='p-4 border-b border-gray-600'>About</li>
